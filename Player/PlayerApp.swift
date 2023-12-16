7e52d11defd329e77fb7dfdc1474c63cb307c720
aleks100s <aleksandr100s@yandex.ru>
2023-12-16 16:07:39 +0700

    Initial Commit

---
+++ Player/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Player
+//
+//  Created by Alexander on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
