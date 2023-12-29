729d8472021834f5fb5832f77cabe2860ca912cd
aleks100s <aleksandr100s@yandex.ru>
2023-12-29 22:38:31 +0700

    Show artwork, artist and title of the audio

--- LocalPackages/Services/Sources/AudioService/AudioServiceImpl.swift
+++ LocalPackages/Services/Sources/AudioService/AudioServiceImpl.swift
@@ -54,13 +54,60 @@ public final class AudioServiceImpl: AudioService {
 	}
 	
 	private func updatePlayer() {
-		MPNowPlayingInfoCenter.default().nowPlayingInfo = [
-			MPMediaItemPropertyTitle: currentFile?.name ?? "-",
-			MPMediaItemPropertyArtist: Bundle.main.infoDictionary?["CFBundleName"] as? String ?? "-",
-			MPMediaItemPropertyPlaybackDuration: NSNumber(value: audioPlayer?.duration ?? 0),
-			MPNowPlayingInfoPropertyElapsedPlaybackTime: NSNumber(value: audioPlayer?.currentTime ?? 0)
-		]
-		MPNowPlayingInfoCenter.default().playbackState = .playing
+		guard let url = currentFile?.url else {
+			Log.debug("Failed to update player for file \(currentFile?.name ?? "???")")
+			return
+		}
+		
+		Task {
+			let asset = AVAsset(url: url)
+			let artwork = try await extractArtwork(from: asset)
+			let title = try await extractStringResource(by: .commonKeyTitle, from: asset) ?? currentFile?.name
+			let artist = try await extractStringResource(by: .commonKeyArtist, from: asset) ?? Bundle.main.infoDictionary?["CFBundleName"] as? String
+			var nowPlayingInfo = [String: Any]()
+			nowPlayingInfo[MPMediaItemPropertyArtwork] = artwork
+			nowPlayingInfo[MPMediaItemPropertyTitle] = title
+			nowPlayingInfo[MPMediaItemPropertyArtist] = artist
+			nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = NSNumber(value: audioPlayer?.duration ?? 0)
+			nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = NSNumber(value: audioPlayer?.currentTime ?? 0)
+			MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+			MPNowPlayingInfoCenter.default().playbackState = .playing
+		}
+	}
+	
+	private func extractArtwork(from asset: AVAsset) async throws -> MPMediaItemArtwork? {
+		let metadata = try await asset.load(.commonMetadata)
+		for item in metadata {
+			if item.commonKey == .commonKeyArtwork {
+				if let data = try await item.load(.value) as? Data {
+					guard let artworkImage = UIImage(data: data) else {
+						Log.debug("Failed to extract artwork from metadata of \(currentFile?.name ?? "???")")
+						break
+					}
+					
+					let artwork = MPMediaItemArtwork(boundsSize: artworkImage.size) { size in
+						return artworkImage
+					}
+					return artwork
+				}
+			}
+		}
+		return nil
+	}
+	
+	private func extractStringResource(
+		by key: AVMetadataKey,
+		from asset: AVAsset
+	) async throws -> String? {
+		let metadata = try await asset.load(.commonMetadata)
+		for item in metadata {
+			if item.commonKey == key {
+				if let title = try await item.load(.value) as? String {
+					return title
+				}
+			}
+		}
+		return nil
 	}
 	
 	private func setupRemoteCommandCenter() {
