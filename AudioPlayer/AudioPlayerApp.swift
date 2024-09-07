2e35424cb644d25d13373fb42e8368b30428d70a
aleks100s <aleksandr100s@yandex.ru>
2024-09-07 20:16:42 +0700

    Initial Commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Alexander on 07.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
