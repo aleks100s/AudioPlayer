8f71c04ec02e5eb0a6672b069b1fc8395cda9b98
aleks100s <aleksandr100s@yandex.ru>
2025-09-26 02:54:21 +0700

    Make detail view a list

--- AudioPlayer/UI/BookDetail/BookDetailView.swift
+++ AudioPlayer/UI/BookDetail/BookDetailView.swift
@@ -9,51 +9,54 @@ import SwiftUI
 
 struct BookDetailView: View {
 	let book: Book
+    
+    @Environment(\.dismiss)
+    private var dismiss
 	
 	var body: some View {
 		NavigationView {
-			ScrollView {
-				LazyVStack(spacing: .zero) {
-					Image(uiImage: book.image)
-						.resizable()
-						.aspectRatio(1, contentMode: .fill)
-						.overlay(alignment: .bottomLeading) {
-							HStack {
-								VStack(alignment: .leading) {
-									Text(book.title)
-										.font(.title)
-									
-									Text(book.author)
-										.foregroundStyle(.secondary)
-								}
-								.padding()
-								
-								Spacer()
-							}
-							.background(.regularMaterial)
-						}
-					
-					VStack {
-						HStack {
-							Text("Количество глав")
-							
-							Spacer()
-							
-							Text("\(book.orderedChapters.count)")
-						}
-						
-						HStack {
-							Text("Длительность книги")
-							
-							Spacer()
-							
-							Text(book.totalDuration)
-						}
-					}
-					.padding()
-				}
-			}
+            List {
+                Image(uiImage: book.image)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(book.title)
+                            .font(.title)
+                        
+                        Text(book.author)
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    Spacer()
+                }
+
+                HStack {
+                    Text("Количество глав")
+                    
+                    Spacer()
+                    
+                    Text("\(book.orderedChapters.count)")
+                }
+                
+                HStack {
+                    Text("Длительность книги")
+                    
+                    Spacer()
+                    
+                    Text(book.totalDuration)
+                }
+            }
 			.navigationTitle("О книге")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Закрыть") {
+                        dismiss()
+                    }
+                }
+            }
 		}
 	}
 }
